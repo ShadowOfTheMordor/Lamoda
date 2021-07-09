015a6910671ffe534cf29c937bce380d0e287455
Maxim Roschin <mroschin2@yandex.ru>
2021-07-10 02:39:16 +0300

    added goods loading from db

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const headerCityButton = document.querySelector(".header__city-button");
-
+let hash = location.hash.substring(1);
 
 headerCityButton.textContent = localStorage.getItem("lamoda-location") || "Ваш город?";
     
@@ -23,11 +23,9 @@ const disableScroll = () => {
         overflow: hidden;
         padding-right: ${widthScroll}px;
     `;
-    // document.body.style.paddingRight = widthScroll;
 };
 const enableScroll = () => {
     document.body.style.cssText = ``;
-    // window.scrollY = document.body.dbScrollY;
     window.scroll({
         top: document.body.dbScrollY
     });
@@ -49,6 +47,51 @@ const cartModalClose = () => {
     enableScroll();
 };
 
+
+
+// получение данных из "базы данных"
+const goodsTitle = document.querySelector(".goods__title");
+
+const getData = async () => {
+    const data = await fetch("db.json");
+    if (data.ok) {
+        return data.json();
+    } else {
+        throw new Error(`Данные не были получены, ошибка ${data.status} ${data.statusText}`);
+    }
+};
+
+const getGoods = (callback, value) => {
+    getData()
+        .then((data) => {
+            if (value) {
+                
+                if (value === "men") {
+                    goodsTitle.textContent = "Мужчинам";
+                } else if (value === "women") {
+                    goodsTitle.textContent = "Женщинам";
+                } else {
+                    goodsTitle.textContent = "Детям";
+                }
+                callback(data.filter((item) => item.category === value));
+            } else {
+                callback(data);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+// getGoods((data) => {
+//     console.warn(data);
+// });
+
+
+
+
+// events
+
 subheaderCart.addEventListener("click", cartModalOpen);
 
 cartOverlay.addEventListener("click", (event) => {
@@ -59,3 +102,55 @@ cartOverlay.addEventListener("click", (event) => {
         cartModalClose();
     }
 });
+
+
+// типа защита
+try {
+    const goodsList = document.querySelector(".goods__list");
+    if (!goodsList) {
+        throw "This is not a goods page!";
+    }
+    const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+        //создание карточки товара
+        // console.log(data);
+
+        const li = document.createElement("li");
+        li.classList.add("goods__item");
+        li.innerHTML = `
+                        <article class="good">
+                            <a class="good__link-img" href="card-good.html#${id}">
+                                <img class="good__img" src="goods-image/${preview}" alt="preview ${name}">
+                            </a>
+                            <div class="good__description">
+                                <p class="good__price">${cost} &#8381;</p>
+                                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                                ${sizes ?
+                                    `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(" ")}</span></p>` :
+                                    ``
+                                }
+                                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                            </div>
+                        </article>
+        `;
+                                // <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes?.join(" ")}</span></p>
+
+        return li;
+    };
+
+    const renderGoodsList = (data) => {
+        goodsList.textContent = "";
+        data.forEach((item) => {
+            const card = createCard(item);
+            goodsList.append(card);
+        });
+    };
+
+    window.addEventListener("hashchange", () => {
+        hash = location.hash.substring(1);
+        getGoods(renderGoodsList, hash);
+    });
+
+    getGoods(renderGoodsList, hash);
+} catch (error) {
+    console.warn(error);
+}
